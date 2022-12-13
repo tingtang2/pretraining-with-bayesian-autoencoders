e37737570a500e6790ac6723d5d369f18e0ea6a3
Ting Chen <zc2666@columbia.edu>
2022-12-13 04:10:59 +0000

    fix loss and actually optimize now

--- src/trainers/sa_vae_trainer.py
+++ src/trainers/sa_vae_trainer.py
@@ -8,6 +8,9 @@ from trainers.vae_trainer import VAENotMNIST2MNISTTrainer
 
 from models.vae import SA_VAE
 
+from tqdm import trange
+import logging
+
 
 def variational_loss(input: Tuple,
                      img,
@@ -19,7 +22,7 @@ def variational_loss(input: Tuple,
     preds = model.decoder(z_samples)
     nll = criterion(preds, img)
     kl = model.kl
-    return nll + kl
+    return nll - kl
 
 
 class SA_VAENotMNIST2MNISTTrainer(VAENotMNIST2MNISTTrainer):
@@ -37,15 +40,14 @@ class SA_VAENotMNIST2MNISTTrainer(VAENotMNIST2MNISTTrainer):
                                              amsgrad=True)
 
         update_params = list(self.model.decoder.parameters())
+        self.max_grad_norm = 5
         self.meta_optimizer = OptimN2N(loss_fn=variational_loss,
                                        model=self.model,
                                        model_update_params=update_params,
-                                       max_grad_norm=5)
-        self.max_grad_norm = 5
+                                       max_grad_norm=self.max_grad_norm)
 
     def train(self, loader: DataLoader):
         self.model.train()
-        running_loss = 0.0
         train_nll_vae = 0.
         train_kl_vae = 0.
         train_nll_svi = 0.
@@ -58,7 +60,7 @@ class SA_VAENotMNIST2MNISTTrainer(VAENotMNIST2MNISTTrainer):
             z, mu, sigma = self.model.encoder(x)
             x_hat = self.model.decoder(z)
             nll_vae = self.criterion(x_hat, x)
-            train_nll_vae += nll_vae * self.batch_size
+            train_nll_vae += nll_vae
             kl_vae = self.model.encoder.kl
             train_kl_vae += kl_vae
 
@@ -71,14 +73,14 @@ class SA_VAENotMNIST2MNISTTrainer(VAENotMNIST2MNISTTrainer):
             assert sigma_svi.requires_grad == True
 
             var_params_svi = self.meta_optimizer.forward(
-                input=[mu_svi, sigma_svi], y=x)
+                input=[mu_svi, sigma_svi], y=x, verbose=i % 10000 == 0)
             mean_svi_final, logvar_svi_final = var_params_svi
 
             z_samples = self.model.sample(mean_svi_final, logvar_svi_final)
             preds = self.model.decoder(z_samples)
 
             nll_svi = self.criterion(preds, x)
-            train_nll_svi += nll_svi * self.batch_size
+            train_nll_svi += nll_svi
             kl_svi = self.model.kl
             train_kl_svi += kl_svi
 
@@ -88,25 +90,86 @@ class SA_VAENotMNIST2MNISTTrainer(VAENotMNIST2MNISTTrainer):
 
             # perform backwards through SVI to VAE
             var_param_grads = self.meta_optimizer.backward(
-                [mean_svi_final.grad, logvar_svi_final.grad])
+                [mean_svi_final.grad, logvar_svi_final.grad],
+                verbose=i % 10000 == 0)
             var_param_grads = torch.cat(var_param_grads, 1)
             var_params.backward(var_param_grads)
 
             if self.max_grad_norm > 0:
                 torch.nn.utils.clip_grad_norm_(self.model.parameters(),
                                                self.max_grad_norm)
+            self.optimizer.step()
 
-        return train_nll_vae / (len(loader) * loader.batch_size)
+        return [
+            metric / (len(loader) * loader.batch_size) for metric in
+            [train_nll_vae, train_kl_vae, train_nll_svi, train_kl_svi]
+        ]
 
     def eval(self, loader: DataLoader) -> float:
-        predictive_ELBO = 0.0
+        total_nll_vae = 0.
+        total_kl_vae = 0.
+        total_nll_svi = 0.
+        total_kl_svi = 0.
 
         self.model.eval()
-        with torch.no_grad():
-            for i, (x, y) in enumerate(loader):
-                x_hat = self.model(x)
-                ELBO_batch = -self.criterion(x_hat, x) + self.model.encoder.kl
+        for i, (x, y) in enumerate(loader):
+            self.optimizer.zero_grad()
 
-                predictive_ELBO += ELBO_batch.item()
+            # normal VAE forward
+            z, mu, sigma = self.model.encoder(x)
+            x_hat = self.model.decoder(z)
+            nll_vae = self.criterion(x_hat, x)
+            total_nll_vae += nll_vae
+            kl_vae = self.model.encoder.kl
+            total_kl_vae += kl_vae
 
-        return predictive_ELBO / (len(loader) * loader.batch_size)
+            # forward SVI steps
+            mu_svi = mu
+            sigma_svi = sigma
+
+            var_params_svi = self.meta_optimizer.forward(
+                input=[mu_svi, sigma_svi], y=x)
+            mean_svi_final, logvar_svi_final = var_params_svi
+
+            z_samples = self.model.sample(mean_svi_final, logvar_svi_final)
+            preds = self.model.decoder(z_samples)
+
+            nll_svi = self.criterion(preds, x)
+            total_nll_svi += nll_svi
+            kl_svi = self.model.kl
+            total_kl_svi += kl_svi
+
+            # compute loss using VAE + SVI variational param
+            var_loss = nll_svi + kl_svi
+
+        return [
+            metric / (len(loader) * loader.batch_size) for metric in
+            [total_nll_vae, total_kl_vae, total_nll_svi, total_kl_svi]
+        ]
+
+    def pretrain(self):
+        train_loader, valid_loader = self.create_pretraining_dataloaders()
+
+        training_elbo = []
+        predictive_elbo = []
+
+        for i in trange(self.pretrain_epochs):
+            train_nll_vae, train_kl_vae, train_nll_svi, train_kl_svi = self.train(
+                train_loader)
+            training_elbo.append(-train_nll_svi.item() + train_kl_svi.item())
+            eval_nll_vae, eval_kl_vae, eval_nll_svi, eval_kl_svi = self.eval(
+                valid_loader)
+            predictive_elbo.append(-eval_nll_svi.item() + eval_kl_svi.item())
+            logging.info(
+                f'epoch: {i} train_elbo_svi: {training_elbo[-1]:.3f}, train_elbo_vae: {-train_nll_vae.item() + train_kl_vae.item():.3f} predictive_elbo_svi: {predictive_elbo[-1]:.3f}, predictive_elbo_vae: {-eval_nll_vae.item() + eval_kl_vae.item():.3f}'
+            )
+
+        self.save_model(name=self.pretrain_name)
+        self.plot_latent(loader=train_loader, name=self.pretrain_name)
+        self.plot_reconstructed(name=self.pretrain_name)
+        self.save_metrics(training_elbo,
+                          name=self.pretrain_name + '_training_elbo',
+                          phase='pretrain')
+        self.save_metrics(predictive_elbo,
+                          name=self.pretrain_name + '_predictive_elbo',
+                          phase='pretrain')
