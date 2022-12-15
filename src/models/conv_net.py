69da568260701ea91d49533a7d5a119b7e725eab
Ting Chen <zc2666@columbia.edu>
2022-12-15 18:28:09 +0000

    fixed conv net ?

--- src/models/conv_net.py
+++ src/models/conv_net.py
@@ -15,7 +15,7 @@ class ConvNetVAE(nn.Module):
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=2)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=2)
         self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(64*(self.shape-1)**2, 2 * self.latent_dim)
+        self.fc1 = nn.Linear(64 * (self.shape - 1)**2, 2 * self.latent_dim)
         self.relu = nn.ReLU()
         self.scale = nn.Parameter(torch.tensor([0.0]))
 
@@ -24,6 +24,8 @@ class ConvNetVAE(nn.Module):
         self.conv3 = nn.ConvTranspose2d(32, 64, kernel_size=2, stride=2)
         self.conv4 = nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2)
         self.conv5 = nn.ConvTranspose2d(32, 1, kernel_size=1, stride=1)
+        self.criterion = nn.MSELoss(reduction='sum')
+        self.eps = 1e-8
 
     def encode(self, x):
         x = self.relu(self.conv1(x))
@@ -57,6 +59,18 @@ class ConvNetVAE(nn.Module):
         kl_loss = kl_loss.sum(-1)
         return kl_loss
 
+    def new_kl_loss(self, mean, std):
+        sigma_squared = torch.nan_to_num(std**2, nan=.01, neginf=.01)
+        assert torch.all(sigma_squared > 0)
+        assert torch.isinf(torch.log(sigma_squared)).any() == False
+
+        return 0.5 * (1 + torch.log(sigma_squared) - mean**2 -
+                      sigma_squared).sum()
+
+    def new_gaussian_likelihood(self, inputs, outputs):
+        return self.criterion(outputs.reshape((outputs.size(0), -1)),
+                              inputs.reshape((inputs.size(0), -1)))
+
     def gaussian_likelihood(self, inputs, outputs, scale):
         dist = torch.distributions.Normal(outputs, torch.exp(scale))
         log_pxz = dist.log_prob(inputs)
@@ -66,13 +80,24 @@ class ConvNetVAE(nn.Module):
         kl_loss = self.kl_loss(z, mean, std)
         rec_loss = self.gaussian_likelihood(inputs, outputs, self.scale)
 
-        return torch.sum(kl_loss - rec_loss)
+        return torch.sum(kl_loss - rec_loss), rec_loss.sum()
+
+    def new_loss_fn(self, z, inputs, outputs, mean, std):
+        kl_loss = self.new_kl_loss(mean, std)
+        # kl_loss = self.kl_loss(z, mean, std)
+        rec_loss = self.new_gaussian_likelihood(inputs, outputs)
+
+        return torch.sum(-kl_loss + rec_loss), rec_loss.sum()
 
     def forward(self, inputs):
         mean, logvar = self.encode(inputs)
-        std = torch.exp(logvar / 2)
+        logvar = torch.nan_to_num(logvar, nan=.01, neginf=.01)
+
+        std = torch.exp(logvar / 2) + self.eps
+        assert torch.all(std > 0)
         z = self.reparamatrize(mean, std)
         outputs = self.decode(z)
 
-        loss = self.loss_fn(inputs, outputs, z, mean, std)
+        # loss = self.loss_fn(inputs, outputs, z, mean, std)
+        loss = self.new_loss_fn(z, inputs, outputs, mean, std)
         return loss, (outputs, z, mean, std)
